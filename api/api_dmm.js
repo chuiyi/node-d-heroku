e815666f3bb40abec70ed1c2bb75d2895f6e5c27
Chase Lin <chase@jet-link.com.tw>
2018-01-08 17:13:57 +0800

    New api: api/dmm/video
    1. Complete api:
    /api/dmm/video

--- api/api_dmm.js
+++ api/api_dmm.js
@@ -9,10 +9,10 @@ router.all('/query', function(req, res) {
         if (req.query.q) {
             url_search = dmm_parser.parseQueryUrl(req.query.q);
         } else {
-            url_search = dmm_parser.parseQueryUrl('金城アンナ');
+            url_search = dmm_parser.parseQueryUrl('大橋未久');
+            // url_search = dmm_parser.parseQueryUrl('金城アンナ');
             // var url_search = dmm_parser.parseQueryUrl('木下柚花');
             // var url_search = dmm_parser.parseQueryUrl('早乙女ルイ');
-            // var url_search = dmm_parser.parseQueryUrl('大橋未久');
             // var url_search = dmm_parser.parseQueryUrl('神谷姫');
             // var url_search = dmm_parser.parseQueryUrl('麻里梨夏');
             // var url_search = dmm_parser.parseQueryUrl('柚月あい');
@@ -23,7 +23,6 @@ router.all('/query', function(req, res) {
             // http://www.dmm.co.jp/digital/videoa/-/actress/recommend/
             // http://www.dmm.co.jp/digital/videoa/-/actress/=/keyword=a/
         }
-        // console.log('search for: ' + url_search);
         dmm_parser.parseList(url_search, function(videos) {
             res.send(videos);
             // console.log(videos);
@@ -34,6 +33,29 @@ router.all('/query', function(req, res) {
         });
     }
 });
+
+router.all('/video', function(req, res) {
+    if (req.query) {
+        var url_video = 'http://www.dmm.co.jp/digital/videoa/-/detail/=/cid=kawd030/';
+        if (req.query.cid) {
+            url_video = 'http://www.dmm.co.jp/digital/videoa/-/detail/=/cid=' + req.query.cid + '/';
+        }
+        if (req.query.url) {
+            url_video = req.query.url;
+        }
+        dmm_parser.parseVideo(url_video, function(video) {
+            res.send(video);
+        });
+        // dmm_parser.parseList(url_search, function(videos) {
+        //     res.send(videos);
+        // console.log(videos);
+        // var randVideo = videos[getRandomInt(1, videos.length) - 1];
+        // dmm_parser.parseVideo(randVideo.url, function(video) {
+        //     console.log(video);
+        // });
+        // });
+    }
+})
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
