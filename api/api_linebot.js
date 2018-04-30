7292a28870c385c13bd820084cee4341d5cf2d14
Chuiyi Lin <chuiyilin@gmail.com>
2018-04-30 14:24:17 +0800

    add channelId

--- api/api_linebot.js
+++ api/api_linebot.js
@@ -3,16 +3,17 @@ var express = require('express');
 var router = express.Router();
 
 const config = {
-    channelAccessToken: 'SzwQw+G1cSQMyNjLOmDv4Z31S4ggaHjATQJgfHIIaZjr7yb3nZ2k1HC1MPqQVYjkvNqkWP1DIfChR3uT2n70kpKusNqSzAPnw4WkMdrJPpafRb8dzt7bqnfOoRfPmfKqjIlNtZyI3GILUgnB9f+zOAdB04t89/1O/w1cDnyilFU=',
-    channelSecret: '3d9689b03d5cee7f534a89ff9ce0b211'
-};
+	channelId: '1577031194',
+	channelSecret: '3d9689b03d5cee7f534a89ff9ce0b211',
+    channelAccessToken: 'SzwQw+G1cSQMyNjLOmDv4Z31S4ggaHjATQJgfHIIaZjr7yb3nZ2k1HC1MPqQVYjkvNqkWP1DIfChR3uT2n70kpKusNqSzAPnw4WkMdrJPpafRb8dzt7bqnfOoRfPmfKqjIlNtZyI3GILUgnB9f+zOAdB04t89/1O/w1cDnyilFU=',  
+}
 
 const client = new line.Client(config);
 
 router.post('/callback', line.middleware(config), (req, res) => {
     console.log('LINEBOT_CALLBACK');
     Promise
-        .all(req.body.events.map(handleEvent))
+        .all(req.body.events.map(echoEvent))
         .then((result) => res.sendStatus(200).json(result))
         .catch((err) => {
             console.error('LINEBOT_ERROR: ' + err);
@@ -22,7 +23,7 @@ router.post('/callback', line.middleware(config), (req, res) => {
 
 
 // event handler
-function handleEvent(event) {
+function echoEvent(event) {
     console.log('LINEBOT_HANDLE_EVENT');
     if (event.type !== 'message' || event.message.type !== 'text') {
         // ignore non-text-message event
@@ -30,10 +31,10 @@ function handleEvent(event) {
     }
 
     // create a echoing text message
-    const echo = { type: 'text', text: event.message.text };
+    const echoMessage = { type: 'text', text: event.message.text };
 
     // use reply API
-    return client.replyMessage(event.replyToken, echo);
+    return client.replyMessage(event.replyToken, echoMessage);
 }
 
 module.exports = router;
