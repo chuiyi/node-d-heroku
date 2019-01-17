d91c7d52ad14c6fa2d6f211e7a84e29e61ecd9de
Chuiyi Lin <chuiyilin@gmail.com>
2019-01-17 15:11:48 +0800

    小更新

--- model/dmm/dmmStorage.js
+++ model/dmm/dmmStorage.js
@@ -1,7 +1,21 @@
-var sqlite3;
+var Engine = require('tingodb')(),
+    assert = require('assert');
 
-var saveVideo = function(video){
-    
+var file = './dmmStorage.db';
+var db = new Engine.Db(file, {});
+
+// var fs = require("fs");
+// var sqlite3 = require('sqlite3').verbose();
+// var db = new sqlite3.Database(file);
+
+var findVideo = function (video) {
+
 }
 
-module.exports = {};
+var saveVideo = function (video) {
+
+}
+
+module.exports = {
+    saveVideo
+};
